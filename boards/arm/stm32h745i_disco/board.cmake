85ec3c19a8d2dc60bbcb87329c8ebf6e1693ac6a
Raphal <gael.raphal-stg@systerel.fr>
2022-04-07 16:16:45 +0200

    use stmcubeprogrammer to flash the board

--- boards/arm/stm32h745i_disco/board.cmake
+++ boards/arm/stm32h745i_disco/board.cmake
@@ -2,7 +2,6 @@
 # Copyright (c) 2022 Systerel (https://www.systerel.fr)
 # SPDX-License-Identifier: Apache-2.0
 
-board_runner_args(jlink "--device=STM32H745XI" "--speed=4000")
+board_runner_args(stm32cubeprogrammer "--erase" "--port=swd" "--reset=hw")
 
-include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
